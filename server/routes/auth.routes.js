709de756f78cc1139ef390a5bcdb26decc57cc45
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2024-01-01 17:44:43 +0000

    update: reset password route added

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,6 +3,7 @@ const {
   logoutHandler,
   requestOTPHandler,
   verifyOTPHandler,
+  resetPasswordHandler,
 } = require("../controllers/auth.controller");
 const requireUser = require("../middleware/requireUser");
 const { noUser } = require("../middleware/role");
@@ -12,6 +13,7 @@ const authRouter = require("express").Router();
 authRouter.post("/login", noUser, loginHandler);
 authRouter.post("/request-otp", noUser, requestOTPHandler);
 authRouter.post("/verify-otp", noUser, verifyOTPHandler);
+authRouter.post("/reset-password", noUser, resetPasswordHandler);
 
 authRouter.post("/logout", requireUser, logoutHandler);
 
