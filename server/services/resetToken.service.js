b68a1aed95114bb08172b826b8bb288481772d91
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2024-01-01 17:30:37 +0000

    reset token service added

---
+++ server/services/resetToken.service.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto')
+const PrismaService = require("./prisma.service");
+
+const prisma = PrismaService;
+
+async function createResetToken(userId) {
+    const token = crypto.randomBytes(16).toString('hex')
+
+    return await prisma.resetTokens.create({
+        data: {
+            token,
+            userId,
+            expiresAt: new Date(Date.now() + 10 * 60 * 1000).toISOString()
+        }
+    })
+}
+
+async function findResetToken(token) {
+    return await prisma.resetTokens.findFirst({
+        where: {
+            token,
+            valid: true,
+            expiresAt: {
+                gt: new Date(),
+            }
+        }
+    })
+}
+
+async function invalidateResetToken(id) {
+    return await prisma.resetTokens.update({
+        where: {
+            id,
+        },
+        data: {
+            valid: false
+        }
+    })
+}
+
+module.exports = {
+    findResetToken,
+    createResetToken,
+    invalidateResetToken,
+}
